93ad246d099026a2ddd12ebc6d5ff141a4528437
Guillaume Arnaud <guillaume.arnaud@externe.systeme-u.fr>
2026-03-25 08:59:30 +0100

    fix: Ensure Terraform binary is downloaded and installed in bootstrap script

--- simple_vm/bootstrap.sh
+++ simple_vm/bootstrap.sh
@@ -13,3 +13,8 @@ Host github.com
 EOF
 
 git config --global url."git@github.com:".insteadOf "https://github.com/"
+
+curl -fsSL https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -o terraform.zip \
+  && unzip terraform.zip \
+  && mv terraform /usr/local/bin/ \
+  && rm terraform.zip
